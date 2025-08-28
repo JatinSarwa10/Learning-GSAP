dfa3e85d4460090e6084913bedee4c619994037d
JatinSarwa10 <sarwajatin@gmail.com>
2025-08-29 00:08:41 +0530

    modify text-animation

--- Text-animation/script.js
+++ Text-animation/script.js
@@ -3,20 +3,33 @@ var h1 = document.querySelector("h1")
 var h1text = h1.textContent
 
 var splittedText = h1text.split("")
+var halfValue = splittedText.length/2;
 
 var clutter = ""
 
-splittedText.forEach(function(elem){
-    clutter += `<span>${elem} </span>`
+splittedText.forEach(function(elem , idx ){
+    if(idx < halfValue){
+        clutter += `<span class = "a">${elem}</span>`
+    }else{
+        clutter += `<span class= "b">${elem}</span>`
+    }
 })
 h1.innerHTML = clutter
 
 }
 breakTheText()
 
-gsap.from("h1 span",{
-    y:100,
+gsap.from("h1 .a",{
+    y:50,
     opacity:0,
-    duration:1,
-    delay:0.5,
+    duration:0.8,
+    delay:0.2,
+    stagger:0.15
+})
+gsap.from("h1 .b",{
+    y:-50,
+    opacity:0,
+    duration:0.8,
+    delay:0.2,
+    stagger:-0.15
 })
