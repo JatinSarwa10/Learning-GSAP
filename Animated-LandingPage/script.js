5e71b1b2149eb88fdc755ebfad7c4392f4f321a1
JatinSarwa10 <sarwajatin@gmail.com>
2025-09-09 00:12:40 +0530

    create page2 animation

--- Animated-LandingPage/script.js
+++ Animated-LandingPage/script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function page1Animation(){
+    var tl = gsap.timeline()
 
 tl.from("nav h1 , nav h4 , nav button",{
     y:-30,
@@ -22,6 +23,7 @@ tl.from(".center-part1 button",{
 })
 tl.from(".center-part2 img",{
     opacity:0,
+    x:200,
 },"-=0.3")
 tl.from(".section1bottom img",{
     y:30,
@@ -29,3 +31,50 @@ tl.from(".section1bottom img",{
     stagger:0.15,
     duration:0.6,
 })
+}
+
+
+function page2Animation(){
+        var tl2 = gsap.timeline({
+        scrollTrigger:{
+            trigger:".section2",
+            scroller:"body",
+            start:"top 50%",
+            end:"top 0%",
+            scrub:2, 
+        }
+    })
+
+    tl2.from(".services",{
+        x:30,
+        opacity:0,
+        duration:0.5,
+    })
+
+    tl2.from(".elem.line1.left",{
+        x:-300,
+        opacity:0,
+        duration:1,
+    },"anim1")
+
+    tl2.from(".elem.line1.right",{
+        x:300,
+        opacity:0,
+        duration:1
+    },"anim1")
+
+    tl2.from(".elem.line2.left",{
+        x:-300,
+        opacity:0,
+        duration:1,
+    },"anim2")
+
+    tl2.from(".elem.line2.right",{
+        x:300,
+        opacity:0,
+        duration:1
+    },"anim2")
+}
+
+page1Animation()
+page2Animation()
